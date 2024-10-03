0dcb240d1cb43bce2e7f8fff30269d52b01e789b
Jesús Enrique Rascón <blograso@gmail.com>
2024-10-03 12:15:29 +0200

    finally fix type

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ["Source Serif Pro"],
+                sans: ['"Source Serif 4 Variable"'],
             },
         },
     },
