e4ae66a86fa3575cb336e1d5014540fde6177c5b
Jesús Enrique Rascón <blograso@gmail.com>
2025-01-26 10:47:24 +0100

    add results

--- src/routes/resultados/+page.svelte
+++ src/routes/resultados/+page.svelte
@@ -1,1 +1,22 @@
+<script>
+    const resultados = [
+        {
+            author: "Carmen Conti Jiménez",
+            title: "Verbos de cambio en español. Alternancia causativa-anticausativa, estructura argumental y unión de predicados subclausales en la Gramática del Papel y la Referencia (RRG)",
+            description:
+                "Comunicación presentada en el LIII Simposio Internacional de la Sociedad Española de Lingüística, celebrado en Salamanca del 20 al 23 de enero de 2025.",
+        },
+    ];
+</script>
+
 <h1>Resultados</h1>
+
+{#each resultados as { author, title, description }}
+    <div
+        class="card border border-stone-700/10 rounded-xl border-primary px-7 backdrop-blur-lg bg-white/1"
+    >
+        <h3>{author}</h3>
+        <h2>{title}</h2>
+        <p>{description}</p>
+    </div>
+{/each}
