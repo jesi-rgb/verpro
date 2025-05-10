24a643cfbefd6f0b4c522c2e771c49bd8d6bde5b
Jesús Enrique Rascón <blograso@gmail.com>
2025-05-10 11:46:03 +0200

    add resultado

--- src/routes/resultados/+page.svelte
+++ src/routes/resultados/+page.svelte
@@ -1,6 +1,12 @@
 <script>
     const resultados = [
         {
+            author: "Enrique Pato",
+            title: "Verbos de posesión, adquisición y transferencia y sujetos no canónicos",
+            description:
+                "Conferencia impartida en el IV Encuentro Internacional ALTYA, celebrado en la Universidad de Jaén los días 28 de abril y 8 de mayo de 2025.",
+        },
+        {
             author: "Carmen Conti Jiménez",
             title: "Verbos de cambio en español. Alternancia causativa-anticausativa, estructura argumental y unión de predicados subclausales en la Gramática del Papel y la Referencia (RRG)",
             description:
@@ -11,12 +17,14 @@
 
 <h1>Resultados</h1>
 
-{#each resultados as { author, title, description }}
-    <div
-        class="card border border-stone-700/10 rounded-xl border-primary px-7 backdrop-blur-lg bg-white/1"
-    >
-        <h3>{author}</h3>
-        <h2>{title}</h2>
-        <p>{description}</p>
-    </div>
-{/each}
+<section class="space-y-10">
+    {#each resultados as { author, title, description }}
+        <div
+            class="card border border-stone-700/10 rounded-xl border-primary px-7 backdrop-blur-lg bg-white/1"
+        >
+            <h3>{author}</h3>
+            <h2>{title}</h2>
+            <p>{description}</p>
+        </div>
+    {/each}
+</section>
